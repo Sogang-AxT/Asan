222b0f593edcb0ee5a4fcafcf9ebf1e75cb3a5c1
Seung-Ki, Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2025-11-13 20:18:01 +0900

    isLeft를 이용한 조건분 분기로 인한 버벅임 문제 확인
    isLeft 변수를 이용해 좌/우 아바타 다리 움직임을 제어하도록 설정함.
    - 노이즈가 심해서 조이콘을 빨리 움직여버리면 isLeft 변수 값이 풀려서 다리가 버벅이듯이 움직임.
    - 해결방안 고민 중...

--- Assets/Scripts/AvatarMovementController.cs
+++ Assets/Scripts/AvatarMovementController.cs
@@ -9,14 +9,24 @@ public class AvatarMovementController : MonoBehaviour {
     [Header("Rotation Settings")]
     [SerializeField] private float maxRotationAngle = 30f; // 최대 회전 각도
     [SerializeField] private float rotationSpeed = 5f; // 회전 속도
+    
+    [SerializeField] private float rotationSmoothTime = 0.1f; // Lerp 대신 SmoothTime 사용
 
     private float _initThighL;
     private float _initThighR;
+    
+    private float _currentZL;  // 현재 Z 각도 추적
+    private float _currentZR;
+    private float _velocityZL; // SmoothDamp용 속도
+    private float _velocityZR;
 
-
+    
     private void Init() {
         this._initThighL = this.thighL.localEulerAngles.z;
         this._initThighR = this.thighR.localEulerAngles.z;
+        
+        this._currentZL = this._initThighL;
+        this._currentZR = this._initThighR;
     }
 
     private void Awake() {
@@ -28,27 +38,35 @@ public class AvatarMovementController : MonoBehaviour {
             return;
         }
         
-        // Propulsion 값을 0~1 범위에서 0~30으로 변환
-        var normalizedRotation = this.playerMovementController.Propulsion * this.maxRotationAngle;
+        
+        // TODO: isLeft만으로는 노이즈가 심해서 빨리 움직이면 조건이 풀려버림
+        // 이 때문에 현재 damp를 쓰나 안쓰나 차이가 없음 (damp는 '바보같은' AI가 추천한 방식)
+
         var isLeft = this.playerMovementController.LeftDominant;
         
         if (isLeft) {   // thighL: 1.52f ~ -28.48f (0~30을 1.52 ~ -28.48로 매핑)
-            var targetZL 
-                = Mathf.Lerp(this._initThighL, (this._initThighL - this.maxRotationAngle), 
-                    normalizedRotation / this.maxRotationAngle);
+            var targetZL = Mathf.Lerp(this._initThighL, (this._initThighL - this.maxRotationAngle),
+                    this.playerMovementController.Propulsion);
+            
+            this._currentZL 
+                = Mathf.SmoothDampAngle(this._currentZL, targetZL, ref this._velocityZL, this.rotationSmoothTime);
+            
             var currentRotationL = this.thighL.localEulerAngles;
-            var newZL = Mathf.LerpAngle(currentRotationL.z, targetZL, Time.deltaTime * this.rotationSpeed);
-
-            this.thighL.localEulerAngles = new Vector3(currentRotationL.x, currentRotationL.y, newZL);
+            // var newZL = Mathf.LerpAngle(currentRotationL.z, targetZL, Time.deltaTime * this.rotationSpeed);
+            
+            this.thighL.localEulerAngles = new Vector3(currentRotationL.x, currentRotationL.y, this._currentZL);
         }
         else {  // thighR: -179.856f ~ -209.856f (0~30을 -179.856 ~ -209.856로 매핑)
-            var targetZR 
-                = Mathf.Lerp(this._initThighR, (this._initThighR - this.maxRotationAngle), 
-                    normalizedRotation / this.maxRotationAngle);
+            var targetZR = Mathf.Lerp(this._initThighR, (this._initThighR - this.maxRotationAngle), 
+                    this.playerMovementController.Propulsion);
+            
+            this._currentZR
+                = Mathf.SmoothDampAngle(this._currentZR, targetZR, ref this._velocityZR, this.rotationSmoothTime);
+            
             var currentRotationR = this.thighR.localEulerAngles;
-            var newZR = Mathf.LerpAngle(currentRotationR.z, targetZR, Time.deltaTime * this.rotationSpeed);
+            // var newZR = Mathf.LerpAngle(currentRotationR.z, targetZR, Time.deltaTime * this.rotationSpeed);
 
-            this.thighR.localEulerAngles = new Vector3(currentRotationR.x, currentRotationR.y, newZR);
+            this.thighR.localEulerAngles = new Vector3(currentRotationR.x, currentRotationR.y, this._currentZR);
         }
     }
 }
